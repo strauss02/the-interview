b45152f89b7a02ea36519ab4af7268dc2ae2a71a
Ido Strauss <strauss02@hotmail.co.il>
2021-11-13 20:49:39 +0200

    add server methods

--- server/app.js
+++ server/app.js
@@ -1,34 +1,34 @@
 /* eslint-disable new-cap */
 
-/** ******** Import tools ******* */
+/* =========== Import tools ========= */
 
 const express = require('express')
 const app = new express()
 const cors = require('cors')
 
-/** ******** Constants ******* */
+/* ========== Constants ======== */
 
-/** ****** Import router modules ******* */
+/* ======== Import router modules ======== */
 
 const errorHandler = require('./error-handler')
 const router = require('./router')
 
-/** ***** General Middleware *********** */
+/* =========== General Middleware ============ */
 
 app.use(cors())
 app.use(express.json())
 
-/** ****** Routing ************ */
+/* ============ Routing ============= */
 
 // app.use(express.static('public'))
 
 app.use('/', router)
 
-/** ****** Error Handler ************ */
+/* ========== Error Handler ============= */
 
 app.use(errorHandler)
 
-/** ************************* */
+/* ============================ */
 
 const PORT = process.env.PORT || 3001
 
--- server/router.js
+++ server/router.js
@@ -3,12 +3,42 @@ const express = require('express')
 const router = express.Router()
 const Question = require('./models/question')
 
+// Return JSON of all questions
 router.get('/list', (req, res) => {
   console.log('hara')
   Question.find({}).then((questions) => {
     console.log(questions)
     res.json(questions)
   })
+})
+
+// Update a question by its ID
+router.put('/update', (req, res) => {
+  // const questionId = req.body.questionId
+  // const newTitle = req.body.newTitle
+  const { newTitle, questionId } = req.body
+  Question.findOneAndUpdate({ _id: questionId }, { title: newTitle }).then(
+    (result) => res.json(result)
+  )
+})
+
+// Create a question
+router.post('/create', (req, res) => {
+  const body = req.body
+
+  const newQuestion = new Question({
+    title: body.title,
+    correctAnswer: body.correctAnswer,
+    answers: body.answers,
+    diffifculty: body.diffifculty,
+  })
+
+  newQuestion
+    .save()
+    .then((savedQuestion) => {
+      res.send('Question saved!')
+    })
+    .catch((err) => res.end(`Error while saving! ${err}`))
 })
 
 module.exports = router
